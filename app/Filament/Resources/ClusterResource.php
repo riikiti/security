3fb022f1516ee646ad8fc7277881520a18c3924d
Ruslan <rik.chanel@gmail.com>
2024-05-19 19:51:26 +0300

    add cluster delete

--- app/Filament/Resources/ClusterResource.php
+++ app/Filament/Resources/ClusterResource.php
@@ -62,6 +62,7 @@ class ClusterResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
