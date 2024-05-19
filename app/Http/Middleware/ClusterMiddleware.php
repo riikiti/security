11aef96a4385f94865063205125d7a6b6e08d4b5
Ruslan <rik.chanel@gmail.com>
2024-05-19 19:21:57 +0300

    fix

--- app/Http/Middleware/ClusterMiddleware.php
+++ app/Http/Middleware/ClusterMiddleware.php
@@ -24,12 +24,11 @@ class ClusterMiddleware
             'cluster_id' => 'required|exists:clusters,id|integer',
             'password' => 'string|required'
         ]);
-        dd($validated_data);
         $cluster = Cluster::query()->where('user_id', auth()->user()->id)->where(
             'id',
             intval($validated_data['cluster_id'])
         )->first();
-
+        dd($cluster);
         if (!Hash::check($validated_data['password'], $cluster->password)) {
             return response()->json([
                 'status' => 403,
