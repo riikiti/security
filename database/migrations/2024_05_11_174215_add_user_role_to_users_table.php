766fa30660558c067a45b1119cad75c4facbe46f
Ruslan <rik.chanel@gmail.com>
2024-06-11 16:45:58 +0300

    find bug

--- database/migrations/2024_05_11_174215_add_user_role_to_users_table.php
+++ database/migrations/2024_05_11_174215_add_user_role_to_users_table.php
@@ -22,7 +22,8 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropForeign('role_id');
+            $table->dropForeign('users_role_id_foreign');
+            $table->dropColumn('role_id');
             $table->dropColumn('is_banned');
         });
     }
